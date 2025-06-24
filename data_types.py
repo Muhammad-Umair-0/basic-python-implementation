9d896b7d1daaf8b90d82b4d986786f5450067750
Muhammad-Umair-0 <Malikmu1321@gmail.com>
2025-06-24 18:05:48 +0500

    small change

--- data_types.py
+++ data_types.py
@@ -3,3 +3,42 @@ print(type(x))
 # list data type 
 x1 = ["apple", "banana", "cherry"]
 print(type(x1))
+
+
+# we can set  specific data type 
+# for string
+X = str("hello word")
+
+#integer
+y= int(20)
+
+print(type(x),type(y))
+
+#float
+a = float(20.5)
+
+b= complex(1j)
+
+print(type(b))
+
+
+## actual Values
+x= 1 #int
+y = 2.5  #float
+z = 1j  #complex
+# conersion 
+a = float(x)
+b = int(y)
+c = complex(x)
+
+
+# check data types
+print(a, type(a))
+print(b,(type(b)))
+print(c, type(c))
+
+
+
+## random numbers 
+import random
+print(random.randrange(1,12))
---
+++ string.py
@@ -0,0 +1,55 @@
+# define a string 
+print("Meri_Tech")
+
+#string to a Variable
+a = "MEri_tech"
+print(a)
+
+# Multiline string 
+a = """
+Meri_Tech,
+is a data driven organization,
+we provide the soution to drive from data
+"""
+print(a)
+
+
+#string array
+a = "MEri_TeCh"
+print(len(a))
+print(a[8])
+
+
+#LOOPing  Through string
+for i in a:
+    print(i, end="")
+
+print("/n")
+
+
+## check String 
+
+text = "Meri_Tech,is a data driven organization,we provide the soution to drive from data"
+print("Tech" in text)
+#use if statement 
+if "Tech" in text:
+    print("Yes , 'Tech' is in text")
+
+
+## cheak if  String not in Text
+print("TExt" not in text)
+
+# use if statement
+if "TExt" not in text:
+    print("'TExt' is not present")
+
+
+
+## string slicing
+print(a[2:6])
+
+print(a[:5])
+
+print(a[2:])
+
+print(a[:-1])
