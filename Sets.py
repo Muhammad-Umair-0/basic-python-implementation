e50106f343150f5b7ec95247d046410ddd5eb627
Muhammad-Umair-0 <Malikmu1321@gmail.com>
2025-06-27 16:35:17 +0500

    complet

--- Sets.py
+++ Sets.py
@@ -45,3 +45,74 @@ set1 = {1,2,3,4,4}
 list1 = ['a','b']
 set1.update(list1)
 print(set1)
+
+#remove item 
+set1.remove(2)  # if the item cannot exist i twill remove the item 
+print(set1) 
+
+#Discard Method
+set1.discard(3)
+print(set1)
+set1.discard(3)
+print(set1)
+
+#to rmeove the random item 
+set1.pop()
+print(set1)
+
+#Empty method
+set1.clear()
+print(set1)
+
+# del keyword used to del the set 
+del set1
+# print(set1) # will raise the error
+
+
+# loop sets 
+
+thisset = {"apple", "banana", "cherry"}
+for x in thisset:
+    print(x)
+
+
+## join sets
+a = {"a", "b", "c"}
+b = {1, 2, 3}
+c= {"John", "Elena"}
+d= {"apple", "bananas", "cherry"}
+print("union")
+print(a.union(b))
+print("interscetion")
+print(a.intersection(b))
+# we can use & instead of intersection
+print(a&b)
+print("difference")
+print(a.difference(b))
+print(a-b)
+#difference update
+print("difference update")
+print(a.difference_update(b))
+print("symetric differnce")
+print(a.symmetric_difference(b))
+
+# join mukltiple sets 
+uni = a.union(b,c,d)
+print("Multiple union\n", uni)
+
+# seprate all the sets
+uni = a|b|c|d
+print(uni)
+
+#update set
+
+a.update(b)
+print(a)
+
+# intersection update
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set1.intersection_update(set2)
+
+print(set1)
