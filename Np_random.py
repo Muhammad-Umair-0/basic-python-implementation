748b12459928a4de335a66e9a6715e45fb1f13df
Muhammad-Umair-0 <Malikmu1321@gmail.com>
2025-07-01 17:19:44 +0500

    working

---
+++ Np_random.py
@@ -0,0 +1,19 @@
+import numpy as np
+from numpy import random
+
+
+
+##genrate a number from 1st 100
+x = random.randint(100)
+print(x)
+
+#genrate a random float 
+x = random.rand()
+print(x)
+
+# Genrating the random 1-D array 
+x = random.randint(100, size=(5))
+print(x)
+#Genrate random 2-D array 
+
+
