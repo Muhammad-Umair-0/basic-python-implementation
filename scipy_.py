c9a20e6bc480799673ddf1608c6fe57d84f6ed13
Muhammad-Umair-0 <Malikmu1321@gmail.com>
2025-07-02 20:37:38 +0500

    updated

---
+++ scipy_.py
@@ -0,0 +1,14 @@
+import scipy
+from scipy import constants
+print(constants.liter)
+
+#checking scipy version 
+print("the scipy version is")
+print(scipy.__version__)
+
+print(constants.pi)
+
+print(dir(constants))
+
+
+# Optimizers
