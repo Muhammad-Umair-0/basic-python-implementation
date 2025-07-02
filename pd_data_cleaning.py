cd2325adc65a661f513f9820c766fd917e6ed422
Muhammad-Umair-0 <Malikmu1321@gmail.com>
2025-07-02 17:19:23 +0500

    complete

--- pd_data_cleaning.py
+++ pd_data_cleaning.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
 # read data
 df = pd.read_csv("data.csv")
 print(df.head(5))
@@ -6,5 +8,27 @@ print(df.head(5))
 null_values = df.isnull().sum()
 print(null_values)
 #dropping the null Values
-df  =df.dropna(inplace=True)
+df.dropna(inplace=True)
+print("The data frame is")
+print(df.duplicated())
+
+
+df.drop_duplicates(inplace=True)
 print(df)
+
+# Correlation b\w the data sets
+print(df.corr())
+
+
+#plotting 
+df.plot()
+plt.show()
+
+#scatter plot
+df.plot(kind="scatter", x='Duration', y='Calories')
+plt.title("Duration vs Calories")
+plt.show()
+
+df.plot(kind="scatter", x='Duration', y='Maxpulse')
+plt.title("Duration vs Maxpulse")
+plt.show()
