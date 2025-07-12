f74650bd56b69841972b44f219999e77255d79aa
Muhammad-Umair-0 <Malikmu1321@gmail.com>
2025-07-12 14:24:32 +0500

    update

---
+++ matplotlib_.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([0,6,5,6,71,2,3,4,5,6,7,8,9,10])
+y = np.array([0,256,4,5,6,7,54,7,8,9,10,11,12,13])
+
+plt.plot(x,y,)
+plt.show()
+# plt.savefig('plot.png')  # Save the plot as a PNG file
