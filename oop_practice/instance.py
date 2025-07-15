cfa7e132ce2fff2598b643fe52fa0934583bf155
Muhammad-Umair-0 <Malikmu1321@gmail.com>
2025-07-15 12:50:24 +0500

    update

---
+++ oop_practice/instance.py
@@ -0,0 +1,67 @@
+class User:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def show(self):
+        print(f"Name {self.name}")
+
+    
+
+
+
+obj = User("Umair",24)
+obj.show()
+
+print('Name', getattr(obj, 'name'))
+print('age ', getattr(obj, 'age'))
+
+
+# add dynamically instance variable to class 
+obj.marks = 75
+print(obj.marks)
+
+# dell any object dynamically 
+del obj.marks
+# print(obj.marks)
+
+delattr(obj, 'name')
+# obj.show()
+
+# access instance from another class
+
+class Vehicle:
+    def __init__(self):
+        self.engine = '1500cc'
+    
+class Car(Vehicle):
+    def __init__(self, maxSpeed):
+        super().__init__()
+        self.maxSpeed = maxSpeed
+
+    def Display(self):
+        print(f"Engine : {self.engine}  speed : {self.maxSpeed}")
+
+car_obj = Car(240)
+car_obj.Display()
+
+# to get list of all instance objects 
+
+print(car_obj.__dict__)
+
+
+# class variables and instance variables 
+
+class Student:
+    # class variables
+    school_name = "ABC school"
+    # constructor
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+
+
+obj = Student("mari",25)
+print(obj.school_name)
+print(obj.age)
