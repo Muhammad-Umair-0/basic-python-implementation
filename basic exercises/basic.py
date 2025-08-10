0847d2a1b527bc864cda82d1eeebc3487ed4525e
Muhammad-Umair-0 <Malikmu1321@gmail.com>
2025-08-10 06:18:06 +0500

    update

---
+++ basic exercises/basic.py
@@ -0,0 +1,16 @@
+sum = 0
+multi = 0
+def add_multi(num1,num2):
+    sum = num1 +num2
+    multi = num1 * num2
+    if multi >=1000:
+        return multi
+    else:
+        return sum
+    
+
+print(add_multi(25,5))
+
+
+#display current and previous number with sum
+
